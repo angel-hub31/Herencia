88ad7be1c7187e8c12e48df130e9b58dabab805b
angel-hub31 <moralesmales1@hotmail.com>
2026-04-28 15:20:05 -0500

    Se crea el testAhorros

---
+++ src/com/krakedev/herencia/test/TestAhorros.java
@@ -0,0 +1,32 @@
+package com.krakedev.herencia.test;
+
+import com.krakedev.herencia.Hija;
+import com.krakedev.herencia.Hijo;
+import com.krakedev.herencia.Padre;
+
+public class TestAhorros {
+
+	public static void main(String[] args) {
+		// se crea objeto Padre
+		 Padre p = new Padre(5,2);
+		 p.ahorrar(500);
+		// p.imprimir();
+		 System.out.println("Padre: " + p);
+		 
+		 // se crea objeto Hija
+		 Hija h =new Hija(4,1);
+		 h.ahorrar(500);
+		// h.imprimir();
+		 System.out.println("Hija: " + h);
+		 
+		 // se cree objeto Hijo
+		 Hijo hj=new Hijo(3,4,8);
+		 hj.ahorrar(500);
+		// hj.imprimir();
+		 System.out.println("Hijo: " + hj);
+		 
+		 
+		 
+	}
+
+}
