d8f61bbb08a5173b0c82d5d4ca8255152ed58756
angel-hub31 <moralesmales1@hotmail.com>
2026-04-28 15:41:41 -0500

    se agreaga una frase al final del reto

--- src/com/krakedev/herencia/test/TestAhorros.java
+++ src/com/krakedev/herencia/test/TestAhorros.java
@@ -8,25 +8,27 @@ public class TestAhorros {
 
 	public static void main(String[] args) {
 		// se crea objeto Padre
-		 Padre p = new Padre("Pablo",5,2);
-		 p.ahorrar(500);
+		Padre p = new Padre("Pablo", 5, 2);
+		p.ahorrar(500);
 		// p.imprimir();
-		 System.out.println("Padre: " + p);
-		 
-		 // se crea objeto Hija
-		 Hija h =new Hija("Luis",4,1);
-		 h.ahorrar(500);
+		System.out.println("Padre: " + p);
+
+		// se crea objeto Hija
+		Hija h = new Hija("Luis", 4, 1);
+		h.ahorrar(500);
 		// h.imprimir();
-		 System.out.println("Hija: " + h);
-		 
-		 // se cree objeto Hijo
-		 Hijo hj=new Hijo("Maria",3,4,8);
-		 hj.ahorrar(500);
+		System.out.println("Hija: " + h);
+
+		// se cree objeto Hijo
+		Hijo hj = new Hijo("Maria", 3, 4, 8);
+		hj.ahorrar(500);
 		// hj.imprimir();
-		 System.out.println("Hijo: " + hj);
-		 
-		 
-		 
+		System.out.println("Hijo: " + hj);
+		
+		System.out.println();
+
+		System.out.println("Notodos los hijos ahorran igual ... y en java eso se controla con sobreescritura");
+
 	}
 
 }
