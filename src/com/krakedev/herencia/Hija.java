08f51205f3afadd580aba0a22ff058ffc86d05c5
angel-hub31 <moralesmales1@hotmail.com>
2026-04-28 15:04:09 -0500

    se realiza correcciones

--- src/com/krakedev/herencia/Hija.java
+++ src/com/krakedev/herencia/Hija.java
@@ -3,34 +3,27 @@ package com.krakedev.herencia;
 // extends hace que clse Hija 
 //herede todos los atributos de la clase padre
 public class Hija extends Padre {
-	
 	// se crea costructor heredado de Padre que recibe virtudes y defectos
-	public Hija(int virtudes,int defectos) {
-		super(virtudes,defectos);
-		
+	public Hija(int virtudes, int defectos) {
+		super(virtudes, defectos);
+
 	}
-	
-	
-	//super()    llama al constructor vacio de Padre
-	//public Hija() {
-		//super();
+
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+
+	// super() llama al constructor vacio de Padre
+	// public Hija() {
+	// super();
 //	}
 
 	public void escucharBadBunny() {
 		System.out.println("Escuchando esta musica horrible");
 
 	}
-	
+
 	// Herencia no significa acceso total, sino acceso controlado
-
-
-	@Override
-	public String toString() {
-		return " Defecto:" + getDefectos() + " Virtudes: " + getVirtudes() ;
-	}
-	
-		
-	
-	
 
 }
